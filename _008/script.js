0866eaec77e669fe42fc7772c59fffd4894da937
vitaliiboiko360 <vukelan@gmail.com>
2021-10-19 22:27:33 +0300

    move normals buffer

--- _008/script.js
+++ _008/script.js
@@ -168,6 +168,52 @@ function initBuffers(gl) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
                     gl.STATIC_DRAW);
 
+    // normals
+    //
+    const normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+  
+    const vertexNormals = [
+      // Front
+       0.0,  0.0,  1.0,
+       0.0,  0.0,  1.0,
+       0.0,  0.0,  1.0,
+       0.0,  0.0,  1.0,
+  
+      // Back
+       0.0,  0.0, -1.0,
+       0.0,  0.0, -1.0,
+       0.0,  0.0, -1.0,
+       0.0,  0.0, -1.0,
+  
+      // Top
+       0.0,  1.0,  0.0,
+       0.0,  1.0,  0.0,
+       0.0,  1.0,  0.0,
+       0.0,  1.0,  0.0,
+  
+      // Bottom
+       0.0, -1.0,  0.0,
+       0.0, -1.0,  0.0,
+       0.0, -1.0,  0.0,
+       0.0, -1.0,  0.0,
+  
+      // Right
+       1.0,  0.0,  0.0,
+       1.0,  0.0,  0.0,
+       1.0,  0.0,  0.0,
+       1.0,  0.0,  0.0,
+  
+      // Left
+      -1.0,  0.0,  0.0,
+      -1.0,  0.0,  0.0,
+      -1.0,  0.0,  0.0,
+      -1.0,  0.0,  0.0
+    ];
+  
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals),
+                  gl.STATIC_DRAW);
+
     // indices
     //
     const indices = [
@@ -184,6 +230,7 @@ function initBuffers(gl) {
 
     return {
         position: positionBuffer,
+        normals: normalBuffer,
         textureCoord: textureCoordBuffer,
         indices: indexBuffer,
     };
