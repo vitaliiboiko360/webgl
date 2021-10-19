b5442d7aef8073f636dd1d53b2c1742b3be8e5a0
vitaliiboiko360 <vukelan@gmail.com>
2021-10-19 22:24:13 +0300

    bind normals to array for the shader to use

--- _008/script.js
+++ _008/script.js
@@ -238,6 +238,26 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
     }
 
+    // Tell WebGL how to pull out the normals from
+    // the normal buffer into the vertexNormal attribute.
+    {
+        const numComponents = 3;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexNormal,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexNormal);
+    }
+
     // tell webgl how to pull out the texture coordinates from buffer
     {
         const num = 2; // every coordinate composed of 2 values
