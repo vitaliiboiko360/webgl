5a24f187e22da456428a7485a68173ab737f057c
vitaliiboiko360 <vukelan@gmail.com>
2021-10-21 20:11:08 +0300

    update vertex and fragment shaders

--- _008/script.js
+++ _008/script.js
@@ -18,29 +18,46 @@ function main() {
 
     // draw
 
-    const vsSource = `
+const vsSource = `
     attribute vec4 aVertexPosition;
+    attribute vec3 aVertexNormal;
     attribute vec2 aTextureCoord;
-    
+
+    uniform mat4 uNormalMatrix;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
     varying highp vec2 vTextureCoord;
-    
-    void main() {
-        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        vTextureCoord = aTextureCoord;
+    varying highp vec3 vLighting;
+
+    void main(void) {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vTextureCoord = aTextureCoord;
+
+      // Apply lighting effect
+
+      highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+      highp vec3 directionalLightColor = vec3(1, 1, 1);
+      highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+
+      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+
+      highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+      vLighting = ambientLight + (directionalLightColor * directional);
     }
     `;
     
     const fsSource = `
-    varying highp vec2 vTextureCoord;
+        varying highp vec2 vTextureCoord;
+        varying highp vec3 vLighting;
 
-    uniform sampler2D uSampler;
+        uniform sampler2D uSampler;
 
-    void main() {
-        gl_FragColor = texture2D(uSampler, vTextureCoord);
-    }
+        void main(void) {
+        highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+
+        gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+        }
     `;
 
     const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
