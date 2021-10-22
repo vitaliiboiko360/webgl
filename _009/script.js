d54bd2205ed37f36156e009f5433fcd122dcd4cf
vitaliiboiko360 <vukelan@gmail.com>
2021-10-22 23:09:22 +0300

    add copyvideo

--- _009/script.js
+++ _009/script.js
@@ -97,44 +97,6 @@ const vsSource = `
     requestAnimationFrame(render);
 }
 
-// will set to true when video can be copied to texture
-var copyVideo = false;
-
-function setupVideo(url) {
-  const video = document.createElement('video');
-
-  var playing = false;
-  var timeupdate = false;
-
-  video.autoplay = true;
-  video.muted = true;
-  video.loop = true;
-
-  // Waiting for these 2 events ensures
-  // there is data in the video
-
-  video.addEventListener('playing', function() {
-     playing = true;
-     checkReady();
-  }, true);
-
-  video.addEventListener('timeupdate', function() {
-     timeupdate = true;
-     checkReady();
-  }, true);
-
-  video.src = url;
-  video.play();
-
-  function checkReady() {
-    if (playing && timeupdate) {
-      copyVideo = true;
-    }
-  }
-
-  return video;
-}
-
 function initBuffers(gl) {
     // positions:
     //
@@ -512,4 +474,42 @@ function loadTexture(gl) {
 
 function isPowerOf2(value) {
     return (value & (value - 1)) == 0;
+}
+
+// will set to true when video can be copied to texture
+var copyVideo = false;
+
+function setupVideo(url) {
+  const video = document.createElement('video');
+
+  var playing = false;
+  var timeupdate = false;
+
+  video.autoplay = true;
+  video.muted = true;
+  video.loop = true;
+
+  // Waiting for these 2 events ensures
+  // there is data in the video
+
+  video.addEventListener('playing', function() {
+     playing = true;
+     checkReady();
+  }, true);
+
+  video.addEventListener('timeupdate', function() {
+     timeupdate = true;
+     checkReady();
+  }, true);
+
+  video.src = url;
+  video.play();
+
+  function checkReady() {
+    if (playing && timeupdate) {
+      copyVideo = true;
+    }
+  }
+
+  return video;
 }
