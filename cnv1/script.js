dc666a6ede12fc7edb1e6e85bb5d063fc2cbede4
vitalii b <vukelan@gmail.com>
2022-07-04 22:24:04 +0300

    add lines

--- cnv1/script.js
+++ cnv1/script.js
@@ -1,3 +1,7 @@
+function getRandPoint() {
+    return Math.floor(Math.random() * 150);
+  }
+
 
 function main(){
 const canvas = document.getElementById('canvas');
@@ -6,5 +10,13 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
+
+const idIntervalMovingRect = setInterval(()=>{
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(getRandPoint(), getRandPoint());
+    ctx.lineTo(getRandPoint(), getRandPoint());
+    ctx.stroke();
+}, 1000);
 }
 main();
