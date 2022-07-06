ed8f0278810cfe72e897f2a108e4d1521a854b52
vitalii b <vukelan@gmail.com>
2022-07-06 23:10:34 +0300

    make it work

--- _011/script.js
+++ _011/script.js
@@ -1,19 +1,40 @@
 
 function main() {
 var canvas = document.getElementById("canvas");
-var gl = canvas.getContext("experimental-webgl");
+var gl = canvas.getContext("webgl");
 
 if(gl === null) {
     alert("unable to init webgl");
     return;
 }
 
-var vertexShader = createShaderFromScriptElement(gl, "2d-vertex-shader");
-var fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
-var program = createProgram(gl, [vertexShader, fragmentShader]);
-gl.useProgram(program);
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const vertexShaderSource = `
+attribute vec2 a_position;
+void main() {
+    gl_Position = vec4(a_position, 0, 1);
+}`;
+gl.shaderSource(vertexShader, vertexShaderSource);
+gl.compileShader(vertexShader);
 
-var positionLocation = gl.getAttribLocation(program, "a_position");
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+const fragmentShaderSource = `
+void main() {
+    gl_FragColor = vec4(0,1,0,1);  // green
+}`;
+gl.shaderSource(fragmentShader, fragmentShaderSource);
+gl.compileShader(fragmentShader);
+
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertexShader);
+gl.attachShader(shaderProgram, fragmentShader);
+gl.linkProgram(shaderProgram);
+
+gl.useProgram(shaderProgram);
+
+console.log(gl.getProgramParameter(shaderProgram, gl.LINK_STATUS));
+
+var positionLocation = gl.getAttribLocation(shaderProgram, "a_position");
 
 var buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
